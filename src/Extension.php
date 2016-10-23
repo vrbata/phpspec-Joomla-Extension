693349ecc2f4ea81bb7038085b65bc3512464973
Josef Vrbata <vrbata.josef@gmail.com>
2016-10-23 15:44:44 +0200

    initialize application

--- src/Extension.php
+++ src/Extension.php
@@ -26,5 +26,24 @@ class Extension implements \PhpSpec\Extension
         define('SYMLA_PATH_ROOT', $params['base_path']);
 
         CliBootstrap::bootstrap();
+        
+        $url  = parse_url($params['base_url']);
+        $path = $url['path'] ?? '';
+
+        $_SERVER['HTTP_HOST']    = $url['host'];
+        $_SERVER['HTTPS']        = $url['scheme'] === 'http' ? '' : $url['scheme'];
+        $_SERVER['PHP_SELF']     = $path . '/index.php';
+        $_SERVER['REQUEST_URI']  = $path;
+        $_SERVER['SCRIPT_NAME']  = $path . '/index.php';
+        $_SERVER['QUERY_STRING'] = '';
+
+        $application = \JFactory::getApplication('site');
+        $reflection  = new \ReflectionClass(\JApplicationSite::class);
+        $method      = $reflection->getMethod('initialiseApp');
+
+        $method->setAccessible(true);
+        $method->invoke($application);
+
+        restore_error_handler();
     }
 }
